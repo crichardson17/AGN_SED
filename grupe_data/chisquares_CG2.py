5fe121736d21647a49f1b21824ab156210d32ccd
cgreene11 <cgreene11@elon.edu>
2016-03-13 16:47:59 -0400

    Finished Chi Square code.  The csv files are the data produced

--- grupe_data/chisquares_CG2.py
+++ grupe_data/chisquares_CG2.py
@@ -16,6 +16,7 @@ import numpy as np
 # Define constants
 
 data_file = "asu3.csv"
+
 fv = 1
 
 #Define a function to call our data
@@ -23,6 +24,7 @@ fv = 1
 def data(filename, column):
     results=np.genfromtxt(filename, delimiter=',',skip_header=1)
     return [result[column] for result in results]
+#Define a function to call the fake data that is produced
 
 # Our observed ax, auv, aox
 observedax=data(data_file,2)
@@ -47,7 +49,7 @@ print(a)
 def linfit(x,m,b):
     return m*x+b
 "define our parameters" 
-linparams = curve_fit(linfit,observedax,obsaox)
+linparams = curve_fit(linfit,observedax,observedauv)
 [m,b] = linparams[0]
 print(m,b)
 
@@ -55,53 +57,115 @@ print(m,b)
 
 def quadfit(x,c,d,e):
     return (c*x**2)+d*x+e
-quadparams = curve_fit(quadfit,observedax,obsaox)
+quadparams = curve_fit(quadfit,observedax,observedauv)
 [c,d,e]=quadparams[0]
 print(c,d,e)
 
 #use the functions to create data points
+
+#The expected ax values
 row_expax=[]
 for item in observedax:
     expected_ax=np.array(m*item+b)
     row_expax.append(expected_ax)
-    print(expected_ax)
+    
     
 row_expax2=[]
 for item in observedax:
     expected_ax_quad=np.array(c*item**2+d*item+e)
     row_expax2.append(expected_ax_quad)
-#write these data points to csv file
+    
+row_expax3=[]
+for item in observedax:
+    expected_ax_const=np.array(a)
+    row_expax3.append(expected_ax_const)
+    
+#the expected auv values
+row_expauv=[]
+row_expauv2=[]
+row_expauv3=[]
+for item in observedauv:
+    expected_auv=np.array(m*item+b)
+    row_expauv.append(expected_auv)
+    
+for item in observedauv:
+    expected_auv_quad=np.array(c*item**2+d*item+e)
+    row_expauv2.append(expected_auv_quad)
+    
+for item in observedauv:
+    expected_auv_const=np.array(a)
+    row_expauv3.append(expected_auv_const)
+    
+#The expected aox values
+row_expaox=[]
+row_expaox2=[]
+row_expaox3=[]
+for item in obsaox:
+    expected_aox=np.array(m*item+b)
+    row_expaox.append(expected_aox)
+for item in obsaox:
+    expected_aox_quad=np.array(c*item**2+d*item+e)
+    row_expaox2.append(expected_aox_quad)
+for item in obsaox:
+    expected_aox_const=np.array(a)
+    row_expaox3.append(expected_aox_const)
+#write these data points to csv file.  currently the code is set up to read the data points produced by the linear fit, to change which data points are being read, change the row_expax to row_expax2, etc. 
 with open('expected_ax.csv', 'wb') as myfile:
     wr=csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerow(row_expax)
 
-"""
-1. Check other fits with excel
-2. Alter chisq code below
-3. Create fake date file
-4. Check code with fake data
-"""
+with open('expected_auv.csv','wb') as myfile:
+    wr=csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(row_expauv)
 
-#data_pts = len(obsaox)
-#print size
+with open('expected_aox.csv', 'wb') as myfile:
+    wr=csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(row_expaox)
+    
 
-# Now we take the difference of the observed and expected values for each and square them
 
-#diffax=(np.subtract(observedax,expectedax))/axerr
-#diffauv=np.subtract(obsaox, expaox)
+#Call this file as our data
+f=open('expected_ax.csv')
+ax_data=[list(map(float,rec)) for rec in csv.reader(f,delimiter=',',)]
+expectedax=[]
+for row in ax_data:
+    expectedax.append(row)
+    
+f2=open('expected_auv.csv')
+auv_data=[list(map(float,rec)) for rec in csv.reader(f2,delimiter=',')]
+expectedauv=[]
+for row in auv_data:
+    expectedauv.append(row)
+
+f3=open('expected_aox.csv')
+aox_data=[list(map(float,rec)) for rec in csv.reader(f3,delimiter=',')]
+expectedaox=[]
+for row in aox_data:
+    expectedaox.append(row)
+
+
+data_pts = len(observedax)
+
+
+# Now we take the difference of the observed and expected values for each spectral index and divide them by the uncertainty
+diffax=(np.subtract(observedax,expectedax))/axerr
+diffauv=(np.subtract(observedauv, expectedauv))/auverr
 
 #square the differences
-#diffaxsq=np.power(diffax,4)
-#diffauvsq=np.power(diffauv,4)
+diffaxsq=np.power(diffax,4)
+diffauvsq=np.power(diffauv,4)
 
-#square the uncertainties
-#auverrsq=np.power(auverr,2)
 
-#add the errors and the differences
-#sumerror=np.add(axerrsq,auverrsq)
-#sumdiff=np.add(diffaxsq,diffauvsq)
+#add these two 
+sumdifferences=np.add(diffaxsq,diffauvsq)
+#sum this over all our data points
 
-#divide the two and sum up all the values in the array
-#chisquare=sumdiff
-#print(sum(np.sqrt(chisquare)))
+totdifference=0
+for element in sumdifferences:
+    totdifference+=element
+chisquare= sum(totdifference)
+
+chisquare=np.sqrt(chisquare)
+print chisquare/data_pts
+
 
